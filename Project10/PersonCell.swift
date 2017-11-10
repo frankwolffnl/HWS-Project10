046d8ec23381df329a4ae771b798c7c5ff2505ce
frankwolffnl <frankwolff.nl@gmail.com>
2017-11-10 14:34:35 +0100

    Initial storyboard done with outlet connections.

---
+++ Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Frank Wolff on 10/11/2017.
+//  Copyright © 2017 Frank Wolff. All rights reserved.
+//  Next Page: https://www.hackingwithswift.com/read/10/3/uicollectionview-data-sources
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+	@IBOutlet var imageView: UIImageView!
+	@IBOutlet var name: UILabel!
+}
